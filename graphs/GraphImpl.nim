2e6ecb8c08afa1d1f944842da37afffe63173adf
Pascal Munerot <pascal.munerot@gmail.com>
2016-07-31 20:13:07 +0200

    added sieve test for benchmarking the intsets with primes

---
+++ graphs/GraphImpl.nim
@@ -0,0 +1,28 @@
+#import trees
+#import Buckets
+
+type
+
+  GraphStore = object of RootObj
+
+  SparseGraphStore = object of GraphStore
+
+
+  StringRec = object
+
+  # 1 page = 4096 bytes
+  Chunk = array [1..64, byte]
+  Page  = array [1..64, Chunk]
+
+  IncidenceMatrixStore = object of GraphStore
+    #directed*: bool
+    bidirected*:bool
+    nbVertices*:int
+    nbEdges*:int
+    case directed*:bool
+    of true:
+    undirMatrix: array[int,int]
+    of false:
+    dirMatrix: array[int, int]
+
+    flag* {.bitsize:1.}: cuint
---
+++ graphs/graphs.nimble
@@ -0,0 +1,9 @@
+[Package]
+version       = "0.1.0"
+author        = "Pascal Munerot"
+description   = "A graph library"
+license       = "MIT"
+bin = Graph
+
+[Deps]
+Requires: "nim >= 0.14.2"
