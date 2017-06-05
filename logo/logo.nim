f88e2042fc93588d7de51592d556db3bad50508b
Pascal Munerot <pascal.munerot@gmail.com>
2017-06-05 18:44:05 +0200

    now working as a class

--- logo/logo.nim
+++ logo/logo.nim
@@ -1,35 +1,48 @@
-## Bare-bones SDL2 example
 import sdl2, sdl2/gfx
 
-discard sdl2.init(INIT_EVERYTHING)
+type
+  LogoEngine* = object
+    window*: WindowPtr
+    render*: RendererPtr
+    fpsman*: FpsManager
 
-var
-  window: WindowPtr
-  render: RendererPtr
+method initEngine*(self:var LogoEngine) {.base.} =
+  discard sdl2.init(INIT_EVERYTHING)
+  self.window = createWindow("Logo", 100, 100, 640,480, SDL_WINDOW_SHOWN)
+  self.render = createRenderer(self.window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+  self.fpsman.init
 
-window = createWindow("SDL Skeleton", 100, 100, 640,480, SDL_WINDOW_SHOWN)
-render = createRenderer(window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+method release*(self:LogoEngine) {.base.} =
+  destroy self.render
+  destroy self.window
+  #destroy self.fpsman ?
 
-var
-  evt = sdl2.defaultEvent
-  runGame = true
-  fpsman: FpsManager
-fpsman.init
+method run(self:var LogoEngine) {.base.} =
+  var evt = sdl2.defaultEvent
+  self.initEngine()
+  var runGame = true
+  try:
+   while runGame:
+      while pollEvent(evt):
+        if evt.kind == QuitEvent:
+          runGame = false
+          break
 
-while runGame:
-  while pollEvent(evt):
-    if evt.kind == QuitEvent:
-      runGame = false
-      break
+      let dt = self.fpsman.getFramerate() / 1000
 
-  let dt = fpsman.getFramerate() / 1000
+      self.render.setDrawColor 220,210,121,255
+      self.render.clear
+      self.render.aacircleColor(100,100,10, 0xfacddefc'u32)
+      self.render.present
+      self.fpsman.delay
 
-  render.setDrawColor 214,2,121,255
-  render.clear
-  render.aacircleColor(100,100,10, 0xfacddefc'u32)
+  finally:
+    self.release()
 
-  render.present
-  fpsman.delay
+proc newEngine*() : LogoEngine =
+  result = LogoEngine()
 
-destroy render
-destroy window
+
+var lge = newEngine()
+lge.run()
+
