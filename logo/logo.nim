db80c46e21ff971a13a1fd4b3afa64685a5d5423
Pascal Munerot <pascal.munerot@gmail.com>
2017-06-07 00:11:17 +0200

    first tokenizer, a few more calls for drawing

--- logo/logo.nim
+++ logo/logo.nim
@@ -1,10 +1,24 @@
-import sdl2, sdl2/gfx
+import sdl2, sdl2/gfx, pegs, strutils
+
+
+#const TOKENS = "[]:?"
+const KEYWORDS = @["[", "]", ":", "?", "end", "forward", "if", "output", "print", "repeat", "reverse", "right",  "to"]
 
 type
+
+  Instruction* = object
+    opcode:char
+
+  LogoParser* = object
+    source* : string
+    #compiled*: Array<Instruction>
+
   LogoEngine* = object
     window*: WindowPtr
     render*: RendererPtr
     fpsman*: FpsManager
+    penForecolor* : uint32
+    penBackcolor* : uint32
 
 method initEngine*(self:var LogoEngine) {.base.} =
   discard sdl2.init(INIT_EVERYTHING)
@@ -32,7 +46,11 @@ method run(self:var LogoEngine) {.base.} =
 
       self.render.setDrawColor 220,210,121,255
       self.render.clear
-      self.render.aacircleColor(100,100,10, 0xfacddefc'u32)
+      self.render.arcColor(200,200,100,10,10, 0xfacddefc'u32)
+      self.render.aacircleColor(100,100,100,  0xda42ac65'u32)
+      for n in 1'i16..10'i16:
+        self.render.aacircleColor(n*30,n*20,n*3, 0x42da65ac'u32)
+
       self.render.present
       self.fpsman.delay
 
@@ -42,6 +60,33 @@ method run(self:var LogoEngine) {.base.} =
 proc newEngine*() : LogoEngine =
   result = LogoEngine()
 
+proc newParser*() : LogoParser =
+  result = LogoParser()
+
+method tokenize*(self:LogoParser, program:string): seq[string] {.base.} =
+  result = newSeq[string]()
+  var tokens = split(program, peg" \s+ / '[' / ']' / '?' / ':' / '<' / '>' / '='")
+  for tok in tokens:
+    result.add(tok)
+
+method parse*(self:LogoParser, tokens:seq[string]): seq[Instruction] {.base.} =
+  result = newSeq[Instruction]()
+  for sym in tokens:
+    result.add(Instruction())
+
+
+let program="""repeat 4 [
+right 10
+forward 20  ]
+if a > 100 [print '*' ]
+"""
+
+var lgp = newParser()
+var tokens = lgp.tokenize(program)
+echo tokens
+
+#var code = lgp.parse(tokens)
+#echo code
 
 var lge = newEngine()
 lge.run()
---
+++ logo/samplepeg.nim
@@ -0,0 +1,63 @@
+import
+  parseopt2, pegs,
+  algorithm, sequtils, strutils, tables
+
+const usageString =
+  """Usage: wordcount [OPTIONS] [FILES]
+
+Options:
+    -o:NAME             set output file name
+    -i --ignore-case    ignore case
+    -h --help           print this help menu
+"""
+
+proc doWork(inFilenames: seq[string] = nil,
+            outFilename: string = nil,
+            ignoreCase: bool = false) {.raises: [IOError,KeyError].} =
+  # Open files
+  var
+    infiles: seq[File] = @[stdin]
+    outfile: File = stdout
+  if inFilenames != nil and inFilenames.len > 0:
+    infiles = inFilenames.mapIt(File, (proc (filename: string): File =
+      if not open(result, filename):
+        raise newException(IOError, "Failed to open file: " & filename)
+    )(it))
+  if outFilename != nil and outFilename.len > 0 and not open(outfile, outFilename, fmWrite):
+    raise newException(IOError, "Failed to open file: " & outFilename)
+
+  # Parse words
+  var counts = initCountTable[string]()
+  for infile in infiles:
+    for line in infile.lines:
+      let input = if ignoreCase: line.tolower() else: line
+      let words = try: input.findAll(peg"\w+") except: @[]
+      for word in words:
+        counts.inc(word)
+
+  # Write counts
+  var words = toSeq(counts.keys)
+  sort(words, cmp)
+  for word in words:
+    outfile.writeln(counts[word], '\t', word)
+
+proc main() =
+  # Parse arguments
+  var inFilenames: seq[string] = @[]
+  var outFilename: string = nil
+  var ignoreCase = false
+  for kind, key, val in getopt():
+    case kind
+    of cmdArgument:
+      inFilenames.add(key)
+    of cmdShortOption, cmdLongOption:
+      case key
+      of "help", "h": echo usageString; return
+      of "ignore-case", "i": ignoreCase = true
+      of "o": outFilename = val
+      else: discard
+    of cmdEnd: discard
+  doWork(inFilenames, outFilename, ignoreCase)
+
+when isMainModule:
+  main()
