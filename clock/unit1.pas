cfd72e02ecf1c826586d13c23b79582dd407bc0b
Pascal Munerot <pascal.munerot@gmail.com>
2017-10-08 23:33:32 +0200

    maj1

--- clock/unit1.pas
+++ clock/unit1.pas
@@ -5,10 +5,19 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
 
 type
-  TForm1 = class(TForm)
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    TimeLabel: TLabel;
+    Timer1: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure TimeLabelClick(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
     { private declarations }
   public
@@ -16,11 +25,30 @@ type
   end;
 
 var
-  Form1: TForm1;
+  MainForm: TMainForm;
 
 implementation
 
 {$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  Width:=100;
+  Height:=48;
+
+end;
+
+procedure TMainForm.TimeLabelClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.Timer1Timer(Sender: TObject);
+begin
+  TimeLabel.Caption:=TimeToStr(Time);
+end;
 
 end.
 
