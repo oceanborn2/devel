7332d9d167373c645a4952ee76c4c2b16b6f0fa0
Pascal Munerot <pascal.munerot@gmail.com>
2018-11-25 01:59:36 +0100

    good version mostly working (except for dataframes and pretty printing)

--- tools/packt/convert.jl
+++ tools/packt/convert.jl
@@ -16,6 +16,7 @@ using DataFrames
 using Dates
 using Unicode
 
+#typealias NullableDate Union{Missing, Dates.Date} end
 
 struct BookInfo
     bookTitle::String
@@ -23,66 +24,64 @@ struct BookInfo
     invoiceUrl::String
     orderPrice::Float32
     orderStatus::String
-    orderDate::String #Date
+    orderDate::String #Union{Missing, Dates.Date}
     idx::UInt16
 end
 
-"""
-<div class="product-line unseen">
-    <div class="product-top-line">
-        <div class="float-left order-info">
-            <div class="order-reference float-left"><a
-                    href="https://www.packtpub.com/account/view_order/28759079">PAC-18-28759079-1090090</a>
-            </div>
-            <div class="order-date float-left">22 November 2018</div>
-            <div class="order-price float-left">£0.00</div>
-            <div class="order-status float-left">Shipped</div>
-        </div>
-        <div class="float-left toggle-product-line">+</div>
-    </div>
-    <div class="toggle order-product-list" style="display:none">
-        <div class="order-product-item cf">
-            <div class="order-product-item-inner">
-                <a href="https://www.packtpub.com/networking-and-servers/windows-server-2016-cookbook">1
-                    x Windows Server 2016 Cookbook [eBook]</a></div>
-        </div>
-    </div>
-</div>
-"""
-
 function isbad(c::Char) return c in ['£','€', '_'] end
 
-function displayRec(orderDiv, counter, items)
+function builddf(dt)
+    fc = fieldcount(BookInfo)
+    fields = map(idx -> (fieldname(BookInfo, idx), fieldtype(BookInfo, idx)), 1:fc)
+    # fields = zip(fieldname(BookInfo, 1), fieldtype(BookInfo, 1))
+    d = DataFrame(dt, fields)
+end
+
+function displayRec(orderDiv, counter, dt)
     if typeof(orderDiv) == HTMLElement{:div}
         classN = get(orderDiv.attributes, "class", "")
         if classN == "product-line unseen"
             productTopLine = orderDiv.children[1]
             orderInfo = productTopLine.children[1]
-            orderRef = orderInfo.children[1]
-            bookURL = string(orderRef.children[1].attributes["href"]) # href
-            bookTitle = string(orderRef.children[1].children[1])
-            invoiceURL = ""
-            orderDate = ""
-            try orderDate = parse(Date, orderInfo.children[2].text) catch e end
+            #println("orderInfo: ", orderInfo)
+            orderRefInfo   = orderInfo.children[1].children[1]
+            #println("orderRefInfo:", string(orderRefInfo))
+            invoiceURL = string(orderRefInfo.attributes["href"]) # text
+            orderRef = string(orderRefInfo.children[1])
+            #println("order ref:", orderRef)
+            #println("invoice url:", invoiceURL)
 
-            orderPriceTemp = (string(orderInfo.children[3].children[1]))
+            orderDate = string(orderInfo.children[2].children[1])
+            #try orderDate = parse(Date,  catch e end
+            #println("orderDate: ", orderDate)
+
+            orderPriceTemp = string(orderInfo.children[3].children[1])
             if orderPriceTemp == ""
                 orderPriceTemp = "_0.00"
             end
-            orderCurrency = SubString(orderPriceTemp,1,1) #Unicode.graphemes(orderPriceTemp).s.SubString(orderPriceTemp,1,1)
+            orderCurrency = SubString(orderPriceTemp,1,1)
             orderPriceTemp = lstrip(isbad, orderPriceTemp)
-            # a = Iterators.Stateful(orderPriceTemp)
-            # popfirst!(a)
-            #replace(orderPriceTemp, "$(orderCurrency)", count=1) #.eachMatch
             orderPrice = parse(Float32, strip(orderPriceTemp))
+            #println("orderPrice: ", orderPrice)
 
-            orderStatus = string(orderInfo.children[4])
+            orderStatus = string(orderInfo.children[4].children[1])
+            #println("orderStatus: ", orderStatus)
 
             productTopList = orderDiv.children[2]
-            titleInfo = productTopList.children[1] #2 causes uncontrolled array out of bound in C layer
-            bookTitle = strip(string(titleInfo.children[1].children))
+            productInfo = productTopList.children[1] #2 causes uncontrolled array out of bound in C layer
 
-            for item in titleInfo.children
+            bookInfo = productInfo.children[1]
+
+            if length(bookInfo.children)>1
+                println("***", counter)
+            end
+
+            for item in bookInfo.children
+                bookTitle   = join(split(string(item.children[1])),"-")
+                bookURL = string(item.attributes["href"])
+                #println("bookURL: ", bookURL)
+                #println("bookTitle: ", bookTitle)
+
                 s = BookInfo(
                     bookTitle,
                     bookURL,
@@ -91,16 +90,15 @@ function displayRec(orderDiv, counter, items)
                     orderStatus,
                     orderDate,
                     counter)
-                println(typeof(items)) #(items, s) #val).insert!(s)
-                #println(s)
+#                    println(s)
+                push!(dt, s)
             end
         end
     end
+    dt
 end
 
-
-items = DataFrame()
-items.columns = fieldnames(BookInfo)
+dt = BookInfo[]
 f = open("orders.html")
 lines = read(f, String) # lines = readlines(f) returns an array, not a string
 try
@@ -109,15 +107,10 @@ try
     body = doc.root.children[2]
     for elem in PostOrderDFS(body)
         if typeof(elem) == HTMLElement{:div}
-           #println(dump(elem))
            elemId = get(elem.attributes, "id", "")
-           limit = 0
            if elemId == "orders-list"
                 for orderDiv in elem.children
-                    if limit < 10
-                        displayRec(orderDiv, counter, items)
-                    end
-                    limit += 1
+                    displayRec(orderDiv, counter, dt)
                     counter += 1
                 end
             end
@@ -127,5 +120,7 @@ finally
     if f != Nothing
         close(f)
     end
-    #println(items)
 end
+
+println(dt)
+#items = builddf(dt)
