8018f7f86abf19e107a922b649832b2ee49d3516
Pascal Munerot <pascal.munerot@gmail.com>
2020-03-18 00:01:11 +0100

    - removed inner fastlog module that was not needed - simplified imports and scoping

--- fastlog/src/fastlog/mod.rs
+++ fastlog/src/fastlog/mod.rs
@@ -1,34 +1,42 @@
-pub mod fastlog {
-    use std::fmt::{Display, Error, Formatter};
-    use std::fmt;
-    use std::ops::Deref;
-    use std::u8;
-    use std::vec::*;
+//pub mod fastlog {
+use std::fmt::{Display, Error, Formatter};
+use std::fmt;
+use std::ops::Deref;
+use std::u8;
+use std::vec::*;
 
-    #[warn(dead_code)]
-    pub enum LevelCode { None = 0, Info = 1, Warn = 2, Error = 4, Fatal = 8, Critical = 16, Debug = 128 }
+#[warn(dead_code)]
+/// Levels to combine as a binary or operation
+pub enum LevelCode { None = 0, Info = 1, Warn = 2, Error = 4, Fatal = 8, Critical = 16, Debug = 128 }
 
-    pub struct Logdata {
-        pub id: u64,
-        pub tstamp: u64,
-        pub level: u8,
-        //LevelCode, from bits union of LevelCode,
-        pub text: &'static str,
-    }
-
-    impl LevelCode {
-        pub fn from(values: Vec<LevelCode>) -> u8 {
-            let mut octet: u8 = 0;
-            for v in values {
-                octet |= v as u8;
-            }
-            octet
+/// build a byte from a vector of levels
+impl LevelCode {
+    pub fn from(levels: Vec<LevelCode>) -> u8 {
+        let mut octet: u8 = 0;
+        for v in levels {
+            octet |= v as u8;
         }
-    }
-
-    impl Display for Logdata {
-        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-            write!(f, "({}, {}, {}, {})", self.id, self.level, self.text, self.tstamp)
-        }
+        octet
     }
 }
+
+pub struct Logdata {
+    /// unique event identifier
+    pub id: u64,
+
+    /// event timestamp
+    pub tstamp: u64,
+
+    ///LevelCode, from bits union of LevelCode,
+    pub levels: u8,
+
+    /// event description
+    pub text: &'static str,
+}
+
+impl Display for Logdata {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {}, {}, {})", self.id, self.levels, self.text, self.tstamp)
+    }
+}
+//}
--- fastlog/src/main.rs
+++ fastlog/src/main.rs
@@ -1,4 +1,6 @@
-use crate::fastlog::fastlog::LevelCode;
+use crate::fastlog::LevelCode;
+use crate::fastlog::LevelCode::*;
+use crate::fastlog::Logdata;
 
 mod fastlog;
 
@@ -7,16 +9,16 @@ mod fastlog;
 fn main() {
     println!("fast logger system");
     //let v = Box<self::Logdata>();
-    let levels = vec!(LevelCode::Debug, LevelCode::Info);
-    let mut v = Box::new(fastlog::fastlog::Logdata {
+    let levels = vec!(Debug, Info);
+    let mut v = Box::new(Logdata {
         id: 1,
         tstamp: 0,
-        level: LevelCode::from(levels),
+        levels: from(levels),
         text: "some text",
     });
     println!("{:}", v);
-    v.id =2;
+    v.id = 2;
     v.text = "another text";
-    v.level = LevelCode::from(vec!(LevelCode::Fatal));
+    v.levels = LevelCode::from(vec!(LevelCode::Fatal));
     println!("{:}", v);
 }
