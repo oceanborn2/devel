e3709334d7ba0fe54ed19fee9c74cd59e23ea89e
Pascal Munerot <pascal.munerot@gmail.com>
2026-04-06 16:37:16 +0200

    remove old files that were moved to channels folder

--- tools/freebox/flatten.jq
+++
@@ -1,2 +0,0 @@
-["mac", "comment", "host", "ip", "ipv4", "ipv6", "active", "persist", "vendor", "host_typ", "interface"], .[]
- | flatten | @csv
--- tools/freebox/query.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -f output.json output.csv
-jq -f query.jq static_lease.json >output.json && jq -r -f flatten.jq output.json >output.csv
